f1d434ad2a59501db186dabb0d94070533814c6f
berhalak <jaroslaw.sadzinski@gmail.com>
2020-02-07 10:20:23 +0100

    Upgrade

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    chainWebpack: config => {
-        config.module
-            .rule('vue')
-            .use('vue-strict')
-            .loader('vue-strict');
-    },    
-    runtimeCompiler: true,
-    css: {
-        loaderOptions : {
-            sass : {
-                data : `@import "@/style/global.scss";`
-            }
-        }
-    }
+	chainWebpack: config => {
+		config.module
+			.rule('vue')
+			.use('vue-strict')
+			.loader('vue-strict');
+	},
+	runtimeCompiler: true,
+	css: {
+		loaderOptions: {
+			sass: {
+				prependData: `@import "@/style/global.scss";`
+			}
+		}
+	}
 }
